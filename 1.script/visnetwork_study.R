998eea23e13f2cb28a11c643c6054cf6499b8ab7
thanxjiny <thanxjiny@gmail.com>
2020-09-03 14:56:57 +0900

    visnetwork_study finish

--- 1.script/visnetwork_study.R
+++ 1.script/visnetwork_study.R
@@ -1,3 +1,9 @@
+# 개인별 사고건수 : node size (value)
+# 보험사기 기적발여부 : node color
+# 개인 / 병원 / 정비업체 : node icon
+# 동일 혐의자간 사고건수 : edge size (value)
+# 탑승 구분 : edge color 
+# 관련 사건 : edge name (label)
 
 
 #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
@@ -35,10 +41,8 @@ vignette("Introduction-to-visNetwork") # with CRAN version
 # shiny example
 shiny::runApp(system.file("shiny", package = "visNetwork"))
 
-
-
 #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-# nodes
+# nodes ------
 #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 
 nodes <- data.frame(id = 1:10,
@@ -73,5 +77,360 @@ nodes <- data.frame(id = 1:10,
 edges <- data.frame(from = c(1,2,5,7,8,10), to = c(9,3,1,6,4,7))
 
 visNetwork(nodes, edges, height = "500px", width = "100%")
+
+# global configuration 
+nodes <- data.frame(id = 1:4)
+edges <- data.frame(from = c(2,4,3,3), to = c(1,2,4,2))
+
+visNetwork(nodes, edges, width = "100%") %>% 
+  visNodes(shape = "square", 
+           color = list(background = "lightblue", 
+                        border = "darkblue",
+                        highlight = "yellow"),
+           shadow = list(enabled = TRUE, size = 10)) %>%
+  visLayout(randomSeed = 12) # to have always the same network  
+
+# global + individual configuration
+nodes <- data.frame(id = 1:4, 
+                    shape = c("circle", "square"), 
+                    label = LETTERS[1:4])
+edges <- data.frame(from = c(2,4,3,3), to = c(1,2,4,2))
+
+visNetwork(nodes, edges, width = "100%") %>% 
+  visNodes(color = list(background = "lightblue", 
+                        border = "darkblue",
+                        highlight = "yellow"),
+           shadow = list(enabled = TRUE, size = 10))  %>%
+  visLayout(randomSeed = 12) # to have always the same network    
+
+# a dot per level 
+nodes <- data.frame(id = 1:3, 
+                    color.background = c("red", "blue", "green"),
+                    color.highlight.background = c("red", NA, "red"), 
+                    shadow.size = c(5, 10, 15))
+
+edges <- data.frame(from = c(1,2), to = c(1,3),
+                    label = LETTERS[1:2], 
+                    font.color =c ("red", "blue"), 
+                    font.size = c(10,20))
+
+visNetwork(nodes, edges)  
+
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+# edges ------
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+edges <- data.frame(from = sample(1:10,8), to = sample(1:10, 8),
+                    
+                    # add labels on edges                  
+                    label = paste("Edge", 1:8),
+                    
+                    # length
+                    length = c(100,500),
+                    
+                    # width
+                    width = c(4,1),
+                    
+                    # arrows
+                    arrows = c("to", "from", "middle", "middle;to"),
+                    
+                    # dashes
+                    dashes = c(TRUE, FALSE),
+                    
+                    # tooltip (html or character)
+                    title = paste("Edge", 1:8),
+                    
+                    # smooth
+                    smooth = c(FALSE, TRUE),
+                    
+                    # shadow
+                    shadow = c(FALSE, TRUE, FALSE, TRUE)) 
+
+# head(edges)
+#  from to  label length    arrows dashes  title smooth shadow
+#    10  7 Edge 1    100        to   TRUE Edge 1  FALSE  FALSE
+#     4 10 Edge 2    500      from  FALSE Edge 2   TRUE   TRUE
+
+nodes <- data.frame(id = 1:10, 
+                    label = paste("Node", 1:10),
+                    group = c("A", "B"))
+visNetwork(nodes, edges, height = "500px", width = "100%")
+
+# global configuration
+
+nodes <- data.frame(id = 1:4)
+edges <- data.frame(from = c(2,4,3,2), to = c(1,2,4,3))
+
+visNetwork(nodes, edges, width = "100%") %>% 
+  visEdges(shadow = TRUE,
+           arrows =list(to = list(enabled = TRUE, scaleFactor = 2)),
+           color = list(color = "lightblue", highlight = "red")) %>%
+  visLayout(randomSeed = 12) # to have always the same network        
+
+# global + individual configuration
+
+nodes <- data.frame(id = 1:4, label = 1:4)
+edges <- data.frame(from = c(2,4,3,2), 
+                    to = c(1,2,4,3), 
+                    dashes = c(TRUE, FALSE))
+
+visNetwork(nodes, edges, width = "100%") %>% 
+  visEdges(shadow = TRUE,
+           arrows =list(to = list(enabled = TRUE, scaleFactor = 2)),
+           color = list(color = "lightblue", highlight = "red")) %>%
+  visLayout(randomSeed = 12) # to have always the same network   
+
+
+nodes <- data.frame(id = 1:3, 
+                    color.background = c("red", "blue", "green"),
+                    color.highlight.background = c("red", NA, "red"), 
+                    shadow.size = c(5, 10, 15))
+
+edges <- data.frame(from = c(1,2), to = c(1,3),
+                    label = LETTERS[1:2], 
+                    font.color =c ("red", "blue"), 
+                    font.size = c(10,20))
+
+visNetwork(nodes, edges) 
+
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+# group ------
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+nodes <- data.frame(id = 1:5, group = c(rep("A", 2), rep("B", 3)))
+edges <- data.frame(from = c(2,5,3,3), to = c(1,2,4,2))
+
+visNetwork(nodes, edges, width = "100%") %>% 
+  # darkblue square with shadow for group "A"
+  visGroups(groupname = "A", color = "darkblue", shape = "square", 
+            shadow = list(enabled = TRUE)) %>% 
+  # red triangle for group "B"
+  visGroups(groupname = "B", color = "red", shape = "triangle")
+
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+# title ------
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+nodes <- data.frame(id = 1:3, group = c("B", "A", "B"))
+edges <- data.frame(from = c(1,2), to = c(2,3))
+
+# default, on group
+visNetwork(nodes, edges, main = "A really simple example", width = "100%")
+
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+# legend ------
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+# default, on group
+visNetwork(nodes, edges, width = "100%") %>%
+  visGroups(groupname = "A", color = "red") %>%
+  visGroups(groupname = "B", color = "lightblue") %>%
+  visLegend()
+
+visNetwork(nodes, edges, width = "100%") %>%
+  visGroups(groupname = "A", color = "red") %>%
+  visGroups(groupname = "B", color = "lightblue") %>%
+  visLegend(width = 0.1, position = "right", main = "Group")
+
+# nodes data.frame for legend
+lnodes <- data.frame(label = c("Group A", "Group B"),
+                     shape = c( "ellipse"), color = c("red", "lightblue"),
+                     title = "Informations", id = 1:2)
+
+# edges data.frame for legend
+ledges <- data.frame(color = c("lightblue", "red"),
+                     label = c("reverse", "depends"), arrows =c("to", "from"))
+
+visNetwork(nodes, edges, width = "100%") %>%
+  visGroups(groupname = "A", color = "red") %>%
+  visGroups(groupname = "B", color = "lightblue") %>%
+  visLegend(addEdges = ledges, addNodes = lnodes, useGroups = FALSE)
+
+ledges <- data.frame(color = c("lightblue", "red"),
+                     label = c("reverse", "depends"), arrows =c("to", "from"))
+
+visNetwork(nodes, edges, width = "100%") %>%
+  visGroups(groupname = "A", color = "red") %>%
+  visGroups(groupname = "B", color = "lightblue") %>%
+  visLegend(addEdges = ledges, useGroups = TRUE) 
+
+# + group + icon + legend
+nodes <- data.frame(id = 1:3, group = c("B", "A", "B"))
+edges <- data.frame(from = c(1,2), to = c(2,3))
+
+visNetwork(nodes, edges) %>%
+  visGroups(groupname = "A", shape = "icon", 
+            icon = list(code = "f0c0", size = 75)) %>%
+  visGroups(groupname = "B", shape = "icon", 
+            icon = list(code = "f007", color = "red")) %>%
+  addFontAwesome() %>%
+  visLegend(addNodes = list(
+    list(label = "Group", shape = "icon", 
+         icon = list(code = "f0c0", size = 25)),
+    list(label = "User", shape = "icon", 
+         icon = list(code = "f007", size = 50, color = "red"))), 
+    useGroups = FALSE)
+
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+# image / icons------
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+# image
+path_to_images <- "https://raw.githubusercontent.com/datastorm-open/datastorm-open.github.io/master/visNetwork/data/img/indonesia/"
+
+nodes <- data.frame(id = 1:4, 
+                    shape = c("image", "circularImage"),
+                    image = paste0(path_to_images, 1:4, ".png"),
+                    label = "I'm an image")
+
+edges <- data.frame(from = c(2,4,3,3), to = c(1,2,4,2))
+
+visNetwork(nodes, edges, width = "100%") %>% 
+  visNodes(shapeProperties = list(useBorderWithImage = TRUE)) %>%
+  visLayout(randomSeed = 2)
+
+
+# fontAwesome icons
+
+nodes <- data.frame(id = 1:3, group = c("B", "A", "B"))
+edges <- data.frame(from = c(1,2), to = c(2,3))
+
+visNetwork(nodes, edges, width = "100%") %>%
+  visGroups(groupname = "A", shape = "icon", 
+            icon = list(code = "f0c0", size = 75)) %>%
+  visGroups(groupname = "B", shape = "icon", 
+            icon = list(code = "f007", color = "red")) %>%
+  addFontAwesome()
+
+# Ionicons
+
+nodes <- data.frame(id = 1:3, shape = "icon", icon.face = 'Ionicons',
+                    icon.code = c("f101", "f100", "f101"))
+edges <- data.frame(from = c(1,2), to = c(2,3))
+
+visNetwork(nodes, edges) %>%
+  addIonicons()
+
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+# options------
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+# highlightNearest
+nb <- 10
+nodes <- data.frame(id = 1:nb, label = paste("Label", 1:nb),
+                    group = sample(LETTERS[1:3], nb, replace = TRUE), value = 1:nb,
+                    title = paste0("<p>", 1:nb,"<br>Tooltip !</p>"), stringsAsFactors = FALSE)
+ # label : node name
+ # value : node size
+ 
+edges <- data.frame(from = c(8,2,7,6,1,8,9,4,6,2),
+                    to = c(3,7,2,7,9,1,5,3,2,9),
+                    value = rnorm(nb, 10), label = paste("Edge", 1:nb),
+                    title = paste0("<p>", 1:nb,"<br>Edge Tooltip !</p>"))
+
+
+ # value : edge size
+
+visNetwork(nodes, edges, height = "500px", width = "100%") %>% 
+  visOptions(highlightNearest = TRUE) %>%
+  visLayout(randomSeed = 123)
+
+# hover options
+visNetwork(nodes, edges, height = "500px", width = "100%") %>% 
+  visOptions(highlightNearest = list(enabled = T, degree = 2, hover = T)) %>%
+  visLayout(randomSeed = 123)
+
+# + image + icons
+visNetwork(nodes, edges, height = "500px", width = "100%") %>% 
+  visGroups(groupname = "A", shape = "icon", 
+            icon = list(code = "f0c0", size = 75)) %>%
+  visGroups(groupname = "B", shape = "icon", 
+            icon = list(code = "f007", color = "red")) %>%
+  visGroups(groupname = "C", shape = "icon", 
+            icon = list(code = "f1b9", color = "black")) %>%
+  visOptions(highlightNearest = list(enabled =TRUE, degree = 2, hover = T)) %>%
+  addFontAwesome() %>%
+  visLayout(randomSeed = 123)
+
+# Collapse / Uncollapse Nodes
+
+nodes <- data.frame(id = 1:15, label = paste("Label", 1:15),
+                    group = sample(LETTERS[1:3], 15, replace = TRUE))
+
+edges <- data.frame(from = trunc(runif(15)*(15-1))+1,
+                    to = trunc(runif(15)*(15-1))+1)
+
+# keeping all parent node attributes  
+visNetwork(nodes, edges) %>% visEdges(arrows = "to") %>%
+  visOptions(collapse = TRUE)
+
+# setting some properties with clusterOptions
+visNetwork(nodes, edges) %>% visEdges(arrows = "to") %>%
+  visOptions(collapse = list(enabled = TRUE, clusterOptions = list(shape = "square"))) 
+
+# enable / disable open cluster (proxy only) : 
+# visEvents(type = "off", doubleClick = "networkOpenCluster")
+# visEvents(type = "on", doubleClick = "networkOpenCluster")
+
+# Data Manipulation
+
+visNetwork(nodes, edges, height = "500px", width = "100%") %>% 
+  visOptions(manipulation = TRUE) %>%
+  visLayout(randomSeed = 123)
+
+
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+# select by node id / group ------
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+# nodesIdSelection
+visNetwork(nodes, edges, height = "500px", width = "100%") %>% 
+  visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE) %>%
+  visLayout(randomSeed = 123)
+
+# on "authorised" column
+visNetwork(nodes, edges, height = "500px", width = "100%") %>%
+  visOptions(selectedBy = "group") %>%
+  visLayout(randomSeed = 123)
+
+# or new column, with multiple groups
+nodes$sample <- paste(sample(LETTERS[1:3], nrow(nodes), replace = TRUE),
+                      sample(LETTERS[1:3], nrow(nodes), replace = TRUE), 
+                      sep = ",")
+nodes$label <- nodes$sample # for see groups
+
+visNetwork(nodes, edges, height = "500px", width = "100%") %>%
+  visOptions(selectedBy = list(variable = "sample", multiple = T)) %>%
+  visLayout(randomSeed = 123)
+
+
+# customized options
+visNetwork(nodes, edges, height = "500px", width = "100%") %>% 
+  visOptions(highlightNearest = TRUE, 
+             nodesIdSelection = list(enabled = TRUE,
+                                     selected = "8",
+                                     values = c(5:10),
+                                     style = 'width: 200px; height: 26px;
+                                 background: #f8f8f8;
+                                 color: darkblue;
+                                 border:none;
+                                 outline:none;')) %>%
+  visLayout(randomSeed = 123)
+
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+# sample “Les miserables” ------
+#■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+nodes <- jsonlite::fromJSON("https://raw.githubusercontent.com/datastorm-open/datastorm-open.github.io/master/visNetwork/data/nodes_miserables.json")
+
+edges <- jsonlite::fromJSON("https://raw.githubusercontent.com/datastorm-open/datastorm-open.github.io/master/visNetwork/data/edges_miserables.json")
+
+
+visNetwork(nodes, edges, height = "700px", width = "100%") %>%
+  visOptions(selectedBy = "group", 
+             highlightNearest = TRUE, 
+             nodesIdSelection = TRUE) %>%
+  visPhysics(stabilization = FALSE)
+
 
 
