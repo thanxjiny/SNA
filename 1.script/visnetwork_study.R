7d1cb4e33074845fa31d15d57b7cefb9c1ee5209
thanxjiny <thanxjiny@gmail.com>
2020-09-10 08:51:50 +0900

    contiune

--- 1.script/visnetwork_study.R
+++ 1.script/visnetwork_study.R
@@ -574,12 +574,63 @@ visIgraph(igraph_network)
 #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 # performance ----
 #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+nnodes <- 2000
+nnedges <- 2000
 
+nodes <- data.frame(id = 1:nnodes)
+edges <- data.frame(from = sample(1:nnodes, nnedges, replace = T),
+                    to = sample(1:nnodes, nnedges, replace = T))
+
+nnodes <- 1000
+nnedges <- 1000
+nodes <- data.frame(id = 1:nnodes, label = paste("Label", 1:nnodes), stringsAsFactors = FALSE)
+edges <- data.frame(from = sample(1:nnodes, nnedges, replace = T),
+                    to = sample(1:nnodes, nnedges, replace = T))
+
+
+# label : node name
+# value : node size
+
+
+# disable or control stabilization using visPhysics 
 visNetwork(nodes, edges) %>%
   visPhysics(stabilization = FALSE)
 
+#disable smooth curve for edges.
 visNetwork(nodes, edges) %>%
   visEdges(smooth = FALSE)
 
+# visigraphlayout
 visNetwork(nodes, edges) %>%
   visIgraphLayout()
+
+visNetwork(nodes, edges, height = "500px", width = "100%") %>% 
+  visOptions(highlightNearest = TRUE, 
+             nodesIdSelection = list(enabled = TRUE,
+                                     selected = "8",
+                                     values = c(5:10),
+                                     style = 'width: 200px; height: 26px;
+                                 background: #f8f8f8;
+                                 color: darkblue;
+                                 border:none;
+                                 outline:none;')) %>%
+  visLayout(randomSeed = 123)
+
+
+
+
+
+nb <- 10
+nodes <- data.frame(id = 1:nb, label = paste("Label", 1:nb),
+                    group = sample(LETTERS[1:3], nb, replace = TRUE), value = 1:nb,
+                    title = paste0("<p>", 1:nb,"<br>Tooltip !</p>"), stringsAsFactors = FALSE)
+# label : node name
+# value : node size
+
+edges <- data.frame(from = c(8,2,7,6,1,8,9,4,6,2),
+                    to = c(3,7,2,7,9,1,5,3,2,9),
+                    value = rnorm(nb, 10), label = paste("Edge", 1:nb),
+                    title = paste0("<p>", 1:nb,"<br>Edge Tooltip !</p>"))
+
+
+
